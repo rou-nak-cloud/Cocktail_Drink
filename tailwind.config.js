0d5b03934c3f5e799599982f3e9b9c2e3e1bf3bd
rou-nak-cloud <github.rounak.19@gmail.com>
2025-06-22 12:16:50 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js,jsx}"], // must keep this for Tailwind to work
+    theme: {
+        extend: {}, // empty now, fine
+    },
+    plugins: [],
+}
