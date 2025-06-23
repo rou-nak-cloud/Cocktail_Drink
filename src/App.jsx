e4ec74c79d6a36d8c1914c46b29df351723ae68f
rou-nak-cloud <github.rounak.19@gmail.com>
2025-06-23 17:46:51 +0530

    mismatch words

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 import React from 'react'
 import Navbar from "./Components/Navbar";
-import Hero from "./Components/hero";
+import Hero from "./Components/Hero";
 import Cocktails from "./Components/Cocktails";
 import About from "./Components/About";
 import Art from "./Components/Art";
